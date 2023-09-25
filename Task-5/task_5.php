1418e5beaf21249f274791a00271a14078301e90
mahamudul001 <mahamuduljalal@gmail.com>
2023-09-25 21:46:04 +0600

    5th task is done

---
+++ Task-5/task_5.php
@@ -0,0 +1,20 @@
+<?php
+
+function generatePassword($length)
+{
+  $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+  $password = '';
+
+  for ($i = 0; $i < $length; $i++) {
+    $password .= $chars[random_int(0, strlen($chars) - 1)];
+  }
+
+  return $password;
+}
+
+// Generate a password of length 12
+$length = 12;
+$generatedPassword = generatePassword($length);
+
+// Print the generated password
+echo "Generated Password: " . $generatedPassword;
