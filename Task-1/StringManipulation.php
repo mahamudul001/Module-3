a07c0aee055a8c07ce255bd21ba429075795cc85
mahamudul001 <mahamuduljalal@gmail.com>
2023-09-25 20:28:52 +0600

    First Task is done

---
+++ Task-1/StringManipulation.php
@@ -0,0 +1,19 @@
+<?php
+
+$text = "The quick brown fox jumps over the lazy dog.";
+
+function modifyText($text) {
+    // Convert the string to all lowercase
+    $text = strtolower($text);
+
+    // Replace "brown" with "red"
+    $text = str_replace("brown", "red", $text);
+
+    echo $text;
+}
+
+
+
+modifyText($text);
+
+?>
